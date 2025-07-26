cf676ed6deec26094b04cfa06e9ca72577425eb2
satyamraj2990 <satyamraj2990@gmail.com>
2025-07-26 12:10:55 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'pink-25': '#fef7f7',
+        'rose-25': '#fef6f6',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+};
